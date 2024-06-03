1446be4921bce156cd9cf7c54f0f3c1c23b34ded
nates linux desktop <nhapeman@gmail.com>
2024-06-02 23:13:39 -0700

    Start time inputs.

--- src/components/FooterControls.tsx
+++ src/components/FooterControls.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./FooterControls.scss"
 import TimeSlider from './TimeSlider';
 
 
 const FooterControls = () => {
+
+    // TODO: add to store.
+    const [selectedDate, setSelectedDate] = useState("2024-05-26")
+    const onDateChange = (event: any) => {
+        setSelectedDate(event.target.value);
+    };
+
+    const [time, setTime] = useState("12:00")
+    const onTimeChange = (event: any) => {
+        setTime(event.target.value);
+    };
+
     return (
         <div style={{ height: "10vh" }} className='FooterControls'>
-            {/* <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
-            <select>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-            </select> */}
+            <div className='date-selection'>
+                <label htmlFor="">Date</label>
+                <input type='date' value={selectedDate} onChange={onDateChange}></input>
+                <label htmlFor="">Time</label>
+                <input type="text" value={time} onChange={onTimeChange} />
+            </div>
             <TimeSlider />
         </div>
     );
