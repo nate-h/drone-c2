db9ea7eb5d83d125cb674961a5ce9ebfba01cda5
nates linux desktop <nhapeman@gmail.com>
2024-12-27 14:54:53 -0800

    Start to make time slider more complex.

--- frontend/src/components/FooterControls.tsx
+++ frontend/src/components/FooterControls.tsx
@@ -10,32 +10,29 @@ import { ReactComponent as ResetIcon } from '../assets/reset-icon.svg'
 
 const FooterControls = () => {
   const timer = useTimer()
-  // TODO: add to store.
-  const [selectedDate, setSelectedDate] = useState('2024-05-26');
-  const onDateChange = (event: any) => {
-    setSelectedDate(event.target.value);
-  };
 
-  // TODO: add to store and hook to slider.
-  const [time, setTime] = useState('12:00');
-  const onTimeChange = (event: any) => {
-    setTime(event.target.value);
-  };
+  // Start time.
+  const [startDate, setStartDate] = useState('2025-01-01');
+  const onStartDateChange = (event: any) => { setStartDate(event.target.value) };
+  const [startTime, setStartTime] = useState('10:00');
+  const onStartTimeChange = (event: any) => { setStartTime(event.target.value) };
+
+  // End time.
+  const [endDate, setEndDate] = useState('2025-01-01');
+  const onEndDateChange = (event: any) => { setEndDate(event.target.value) };
+  const [endTime, setEndTime] = useState('14:00');
+  const onEndTimeChange = (event: any) => { setEndTime(event.target.value) };
 
   return (
     <div className='FooterControls'>
-      <div className='date-selection'>
-        <div className='row'>
-          <div style={{ position: "absolute", left: 5, bottom: 5 }}>{timer.value}</div>
-          <label>
-            Date <input type='date' value={selectedDate} onChange={onDateChange}></input>
-          </label>
-        </div>
-        <div className='row'>
-          <label>
-            Time <input type='text' value={time} onChange={onTimeChange} />
-          </label>
-        </div>
+      <div style={{ position: "absolute", left: 5, bottom: 100, zIndex: 10000 }}>{timer.value}</div>
+      <div className='time-domain'>
+        Start
+        <input type='date' value={startDate} onChange={onStartDateChange}></input>
+        <input type='text' value={startTime} onChange={onStartTimeChange} />
+        End
+        <input type='date' value={endDate} onChange={onEndDateChange}></input>
+        <input type='text' value={endTime} onChange={onEndTimeChange} />
       </div>
       <TimeSlider />
       <ul className='time-controls'>
